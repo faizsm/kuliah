419581577441a1c03eca7c8c2e84c908a12e81ab
faizsm <94114712+faizsm@users.noreply.github.com>
2023-11-13 09:46:31 +0700

    Add files via upload

---
+++ situs/cv.php
@@ -0,0 +1,52 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border='0' width='50%' align="center">
+        <tr>
+            <th colspan="4"> Curikulum Vite</th>
+        </tr>
+        <tr><td>
+            <img src="foto/faiz.jpg" width="100px" height="50px">
+        </td>
+    </tr>
+        <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td>Faiz sahrul muharam</td>
+        </tr>
+        <tr>
+            <td>Tempat,Tanggal Lahir</td>
+            <td>:</td>
+            <td>Tasikmalaya,02 Maret 2004</td>
+        <tr>
+            <td>Jenis Kelamin</td>
+            <td>:</td>
+            <td>Laki-Laki</td>
+        </tr>
+        <tr>
+            <td>Agama</td>
+            <td>:</td>
+            <td>Islam</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>:</td>
+            <td>Jln.cienteung gede no 9A, kecamatan cihideung ,keluharan argasari</td>
+        </tr>
+        <tr>
+            <td>Nomer Telepon</td>
+            <td>:</td>
+            <td>083854482915</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>:</td>
+            <td>faizsm665@gmail.com</td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ situs/index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>faiz</title>
+</head>
+<body>
+    <table border="0" width='50%' bgcolor='red'>
+        <tr>
+            <th >ini head</th>
+        </tr>
+        <table border ='0' width='10%'>
+        <tr>
+            <th>Menu</th>
+            <tr>
+                <td> <a href="index.php">Beranda</a></td>
+            </tr>
+            <tr>
+                <td> <a href="cv.php">CV</a></td>
+            </tr>
+            <tr>
+                <td> <a href="galeri.php">Galeri</a></td>
+            </tr>
+        </tr>
+        <table border="0" width='50%'>
+        <tfoot ><th>foot</th></tfoot>
+        </table>
+
+</body>
+</html>
